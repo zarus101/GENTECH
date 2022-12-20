003ebfb803988ae0e42c31c6ee37d1fbb5c6d660
zarus101 <83441959+zarus101@users.noreply.github.com>
2022-12-20 23:09:31 +0545

    Revert "added demo navbar and artist page"
    This reverts commit d8f4fab60d6474c9a85471def0b61f6b5fe00262.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+<<<<<<< Updated upstream
 import React from "react";
 import Routes from "./Routes";
 import "./assets/index.scss";
+=======
+
+import {  Route, Routes } from "react-router-dom";
+import SideNavbar from "./Components/sideNavbar";
+import Home from "./Pages/Home";
+import "./Assests/index.scss"
+import { useState } from "react";
+import Browse from "./Pages/Browse";
+import Artists from "./Pages/Artists/Artists";
+import Login from "./Pages/login/LoginForm";
+import Register from "./Pages/register/RegisterForm";
+
+
+>>>>>>> Stashed changes
 
 
 function App() {
